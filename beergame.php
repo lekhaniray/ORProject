e769ccf4c2312468ea1191baaa8373de71980cba
Priyansh Jain <jain.priyansh.2210@gmail.com>
2017-10-24 23:01:58 +0530

    Modified

--- beergame.php
+++ beergame.php
@@ -48,7 +48,7 @@ $Incoming_Deliv_F = 0;
 $Init_order = $_post['iOrder'];
 
 // Bullwhip Simulator
-for(i=1;i<N;i++)
+for($i=1;i<N;$i++)
 {
 	echo $i;
 	if ($i<=5) 
@@ -69,7 +69,109 @@ for(i=1;i<N;i++)
     {
     	$Incoming_Deliv_R = $Outgoing_Deliv_W($i-2);
     }
-                                
+    
+    $Incoming_Order_R = $Customer_Demand;   // Order from Customer
+    $c_R = ($Stock_R + $Incoming_Deliv_R) - ($Incoming_Order_R + $Backorder_R);           
+    if($c_R<0)
+    {
+        $Outgoing_Deliv_R = $Stock_R + $Incoming_Deliv_R;
+        $Backorder_R = abs($c_R);
+        $Stock_R=0;
+    }
+    else
+    {
+        $Outgoing_Deliv_R = $Incoming_Order_R + $Backorder_R;                       
+        $Stock_R = $c_R;
+        $Backorder_R = 0;
+    }
+
+    $Total_cost_R = $Backorder_R * 2 + $Stock_R * 1;           
+    $Total_cost_R_array($i) = $Total_cost_R;
+    if($Stock_R > 0)
+    {
+        $Total_stock_R($i) = $Stock_R;
+    }
+    else
+    {
+        $Total_stock_R($i) = - $Backorder_R;
+    }           
+    $Orders_C($i) = $Incoming_Order_R;
+    
+    echo ("Incoming Delivery from provider: ");           
+    echo $Incoming_Deliv_R;
+    echo "<br>";
+    echo ("Incoming Order from client: ");
+    echo $Incoming_Order_R;
+    echo ("Outgoing   Delivery: ");
+    echo $Outgoing_Deliv_R;
+    echo "<br>";
+    echo ("Backorder: ");
+    echo $Backorder_R;
+    echo "<br>";
+    echo ("Stock: ");
+    echo $Stock_R;
+    echo "<br>"
+    echo ("Total   cost: ");
+    echo $Total_cost_R;
+    echo "<br>";
+    $My_Order_R($i) = $_post['Retailer_order'];          
+    $Orders_R($i) = $My_Order_R($i);
+    echo "<br>"; echo "<br>"; echo "<br>";
+
+    // Wholesaler 
+    echo ("WHOLESALER: ");
+    if($i < = 2)
+    {
+        $Incoming_Deliv_W = 0;
+    }
+    else
+    {
+        $Incoming_Deliv_W = $Outgoing_Deliv_D ($i - 2);
+    }
+
+    if($i == 1)
+    {
+        $Incoming_Order_W = $Init_order;
+    }
+    else
+    {
+        $Incoming_Order_W = $My_Order_R($i-1);   // Order   from   retailer
+    }           
+    $c_W = ($Stock_W + $Incoming_Deliv_W) - ($Incoming_Order_W + $Backorder_W);           
+    if($c_W < 0)
+    {
+        $Outgoing_Deliv_W($i) = $Stock_W + $Incoming_Deliv_W;
+        $Backorder_W = abs($c_W);
+        $Stock_W = 0;
+    }    
+    else
+    {
+        $Outgoing_Deliv_W($i) = $Incoming_Order_W + $Backorder_W;                       
+        $Stock_W = $c_W;
+        $Backorder_W=0;
+    } 
+          
+    $Total_cost_W = $Backorder_W*2 + $Stock_W*1;           
+    $Total_cost_W_array($i) = $Total_cost_W;
+    if($Stock_W > 0)
+    {
+        $Total_stock_W($i) = $Stock_W;
+    }
+    else
+    {
+        $Total_stock_W($i) =- $Backorder_W;
+    }          
+    echo("Incoming Delivery from provider: ");           
+    echo $Incoming_Deliv_W;
+    echo "<br>";
+    echo("Incoming   Order   from   client: ");
+    echo $Incoming_Order_W;           
+    echo "<br>";
+    echo("Outgoing Delivery: ");
+    echo $Outgoing_Deliv_W($i);           
+    echo "<br>";
+    echo("Backorder: ");
+    echo $Backorder_W;
+                                 
 }
-843301075995
 ?>
